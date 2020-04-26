7ef092afe02b1cc27b0ed4c37c3abbe44f19a3eb
iamyunika <yupadhayaya08@gmail.com>
2020-04-25 22:53:00 -0500

    found the bottom and swapped

--- convex_hull_graham_scan.c
+++ convex_hull_graham_scan.c
@@ -1,12 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct Point
+/*
+    -> We are usng graham's scan to solve the convex hall problem, 
+       graham's scan solves it by mainting a stack of  S of all the points.
+    -> It pushes each point of the input set onto the stack one time,
+       and then pops from the stack each point which is not a vertex. 
+    -> When, the function finishes stack contains exactly the vertices of 
+       all the convex hull in counterclocl wise direction from their appearance
+       of the boundary.
+*/
+typedef struct Point
 {
     int x;
     int y;
-};
+}Point;
+
+//Point P0;
+
+int minimum_index = 1;
+
+void swap_bottom(int total, Point points[total], int first_index, int second_index)
+{
+    Point temp = points[first_index];
+    points[first_index] = points[second_index];
+    points[second_index] = temp;
+}
+/*
+    This function returns true if there are two tie y-values.
+*/
+bool tie_case(int y_min, int y, int x, int x_min)
+{
+    bool flag = false;
+
+    if(y_min == y)
+    {
+        if(x < x_min)
+        {
+            flag = true;
+        }
+    }
+    return flag;
+}
+
+/*
+    Find the point with minimum y-coordinate.
+*/
+int min_y(int total_points, Point points[total_points])
+{
+    int bottom_y = points[0].y;
+    int i = 1;
+    while(i < total_points)
+    {
+        int point_y = points[i].y;
+        /*
+            Takes into consideration the tie-case: if there are same minimum y-value, 
+            it will return the left-most one.
+        */
+        bool tie = tie_case(bottom_y,point_y,points[i].x,points[minimum_index].x);
+        if((point_y < bottom_y) || (tie == true))
+        {
+            bottom_y = points[i].y;
+            minimum_index = i;
+        }
+        i++;
+    }
+    return bottom_y;
+}
+
+/*
+
+*/
+void convexHull(int total_points, Point points[total_points])
+{
+    int bottom_y = min_y(total_points,points);
+    swap_bottom(total_points, points,0,minimum_index);
+}
 
 int main()
 {
@@ -14,7 +85,7 @@ int main()
     int total_points = 0;;
     printf("\nEnter total points you want to check for: ");
     scanf("%d",&total_points);
-    struct Point arr_points[total_points];
+    Point arr_points[total_points];
     printf("\nEnter two numbers separated by a space for (e.g.: 10 15):\nStop with: 0 0\n");
     for(i = 0; i < total_points; i++)
     {
@@ -28,15 +99,22 @@ int main()
         {
 
         }
-        
     }
 
-    printf("Printing all the points to compute convex hall.\n");
-    printf(" x\t y\n");
-    for(i = 0; i < total_points; i++)
-    {
-        printf("%d\t%d\n",arr_points[i].x,arr_points[i].y);
-    }
-    return 0;
+    // printf("Printing all the points to compute convex hall.\n");
+    // printf(" x\t y\n");
+    // for(i = 0; i < total_points; i++)
+    // {
+    //     printf("%d\t%d\n",arr_points[i].x,arr_points[i].y);
+    // }
+
+    convexHull(total_points,arr_points);
+
+    // printf(" x\t y\n");
+    // for(i = 0; i < total_points; i++)
+    // {
+    //     printf("%d\t%d\n",arr_points[i].x,arr_points[i].y);
+    // }
+    // return 0;
 }
 
